a3dedca8c7cf6289cd42ef9ce9b45c0267075479
Rafael Guterres <guterresrafael@gmail.com>
2018-10-12 19:34:46 -0300

    First commit

---
+++ src/main/java/spring/integration/jms/SpringIntegrationJmsApplication.java
@@ -0,0 +1,20 @@
+package spring.integration.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ *
+ * @author guter
+ */
+@SpringBootApplication
+@PropertySource("classpath:META-INF/spring/integration/application.properties")
+@ImportResource("classpath*:META-INF/spring/integration/application.xml")
+public class SpringIntegrationJmsApplication {
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(SpringIntegrationJmsApplication.class).run(args);
+        System.out.println("SpringIntegrationJmsApplication RUNNING...");
+    }
+}
